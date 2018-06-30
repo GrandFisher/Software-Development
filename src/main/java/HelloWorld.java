455fa141089a8487fbb7610f10ab3e73f97d6345
GrandFisher <470911438@qq.com>
2018-06-30 16:37:17 +0800

    自动构建测试2

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -7,5 +7,6 @@ public class HelloWorld {
     public static void main(String[] args){
         System.out.println("HelloWorld!");
         System.out.println("自动构建");
+        System.out.println("自动构建1");
     }
 }
