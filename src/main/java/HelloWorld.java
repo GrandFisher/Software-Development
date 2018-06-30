f53085e88a7b5829c972a53fe52567fbec84184b
GrandFisher <470911438@qq.com>
2018-06-30 22:17:32 +0800

    自动构建测试

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -4,15 +4,10 @@
  */
 
 public class HelloWorld {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("HelloWorld!");
-        System.out.println("自动构建");
-        System.out.println("自动构建1");
-        System.out.println("自动构建2");
-        System.out.println("自动构建3");
-        System.out.println("自动构建4");
-        System.out.println("自动构建5");
-        System.out.println("自动构建6");
-        System.out.println("自动构建7");
+        for (int i = 0; i <= 10; i++) {
+            System.out.println("自动构建" + i);
+        }
     }
 }
